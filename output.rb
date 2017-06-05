bc4fdedf3c9fda88660e50ef402c36352ec1d4c8
Busyra Anwar <busyra@me.com>
2017-06-05 11:26:04 -0700

    moves read_text under private, adds logic to process, [WIP] adds all commandswith comments

--- output.rb
+++ output.rb
@@ -7,6 +7,44 @@ class Output
     read_text
   end
 
+  def process(command, components)
+    if command == 'DEPEND' then depends(command, components)
+    elsif command == 'INSTALL' then installs(command, components)
+    elsif command == 'REMOVE' then removes(components)
+    elsif command == 'LIST' then lists(components)
+    else
+      end_prog
+    end
+  end
+
+  def depends(command, components)
+    puts 'depending' + components.to_s
+    binding.pry
+    # add to @dependents_hash
+  end
+
+  def installs(command, components)
+    puts 'installing'
+    # check if components already installed
+    # add components to @installed_components
+    # puts output message " Installing " + component
+  end
+
+  def removes(components)
+    puts 'removing'
+    # checks if components already installed_components
+    # checks if components is val to any other component key in @dependents_hash
+    # checks if component key is in @installed_components
+    # removes from component from @installed_components
+  end
+
+  def lists(components)
+    puts 'listing'
+    # puts all @installed_components
+  end
+
+  private
+
   def read_text
     File.open('input.txt').each do |line|
       puts line
@@ -26,12 +64,7 @@ class Output
     components_array
   end
 
-  def process(command, components)
-    puts 'hello world'
-    puts command
-    puts components
-    binding.pry
-  end
+  def end_prog; end
 
   self
 end.new
